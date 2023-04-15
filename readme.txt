49f9385894d0e438320547a2713ab2040745bb0c
BrendaMartinez28 <125223171+BrendaMartinez28@users.noreply.github.com>
2023-04-15 16:06:11 -0300

    Correciones de codigo funciones

---
+++ readme.txt
@@ -0,0 +1,13 @@
+Primera preentrega JS 2023.
+
+//Simulador interactivo, donde buscamos calcular cuando se trabajara en la jornada, dependiendo de los pacientes a atender y teniendo en cuenta que cada consulta dura 30 minutos.
+
+//Correccion de la devolucion 
+
+//Agregue la funcion que se pidio en primero instancia.
+
+//Agregue lo que me aconsejo el profe.
+
+
+
+ 
--- script/main.js
+++ script/main.js
@@ -1,13 +1,23 @@
 //PRE ENTREGA NUMERO 1:
 // SEGUN CUANTO PACIENTES ATENDEREMOS, CALCULAMOS LO QUE TRABAJAREMOS
+function multiplicar(tiempoPaciente, numeroPacientes) {
+    let resultado = tiempoPaciente * numeroPacientes;
+    return resultado;
+}
+
 let numeroPacientes = parseInt(prompt("¿Cuantos pacientes atenderemos hoy? Teniendo en cuanta que cada consulta demora 30 minutos"));
+while (isNaN(numeroPacientes)) {
+    numeroPacientes = parseInt(prompt("¿Cuantos pacientes atenderemos hoy? Teniendo en cuanta que cada consulta demora 30 minutos"));
+}
 for (let turno = 1; turno <= numeroPacientes; turno++) {
-let nombre = prompt("¿Como se llama el paciente " + turno + "?")    
-alert("El paciente "+ nombre + ", tiene asignado el turno numero " + turno);
-console.log("El paciente " + nombre + " , tiene asignado el turno numero " + turno); 
+    let nombre = prompt("¿Como se llama el paciente " + turno + "?")
+    while (nombre === "" || !isNaN(nombre) || nombre === " ") {
+        nombre = prompt("¿Como se llama el paciente " + turno + "?");
+    }
+    alert("El paciente " + nombre + ", tiene asignado el turno numero " + turno);
+    console.log("El paciente " + nombre + " , tiene asignado el turno numero " + turno);
 
 }
 let tiempoPaciente = 30;
-let tiempoDeTrabajo = 0;
-tiempoDeTrabajo = tiempoPaciente * numeroPacientes;
-alert("Segun nuestros calculos aproximadamente trabajaremos " + tiempoDeTrabajo + " minutos.");
+let resultado = multiplicar(tiempoPaciente, numeroPacientes);
+alert("Segun nuestros calculos aproximadamente trabajaremos " + resultado + " minutos.");
